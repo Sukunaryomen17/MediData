383b49597e38352beef3381c2b04741a1c675699
Rishabh Sharma <88846729+blessedrishabh@users.noreply.github.com>
2026-04-12 17:21:04 +0530

    corrected build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,2 @@
-# Add the --break-system-packages flag to bypass the error
-pip install -r requirements.txt --break-system-packages
-
+python3.9 -m pip install -r requirements.txt --break-system-packages
 python3.9 manage.py collectstatic --noinput
