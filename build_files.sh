2208ca340056ccd8430085a76741a8f37465210f
Rishabh Sharma <88846729+blessedrishabh@users.noreply.github.com>
2026-04-12 17:10:50 +0530

    updated build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
-pip install -r requirements.txt
+# Add the --break-system-packages flag to bypass the error
+pip install -r requirements.txt --break-system-packages
+
 python3.9 manage.py collectstatic --noinput
