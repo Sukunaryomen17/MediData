c74001ea7a86e90117ee0baf693cbdb013e259ec
Rishabh Sharma <88846729+blessedrishabh@users.noreply.github.com>
2026-04-12 17:44:49 +0530

    Update STATIC_URL and add STATIC_ROOT

--- myproject/settings.py
+++ myproject/settings.py
@@ -131,7 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Where to go after successful login
 LOGIN_REDIRECT_URL = 'home'
